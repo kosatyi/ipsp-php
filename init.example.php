5812b698aa44df410280b0588bc11b40106e8845
Stepan Kosatyi <stepan@kosatyi.com>
2016-08-23 18:43:57 +0300

    Update init.example.php

--- init.example.php
+++ init.example.php
@@ -1,4 +1,7 @@
 <?php
-require_once 'autoload.php';
-$client = new Ipsp_Client( 1000 ,'test' , 'api.fondy.eu' );
+require_once 'ipsp-php/autoload.php';
+define('MERCHANT_ID' , 1000);
+define('MERCHANT_PASSWORD' , 'test');
+define('IPSP_GATEWAY' ,  'api.fondy.eu');
+$client = new Ipsp_Client( MERCHANT_ID , MERCHANT_PASSWORD, IPSP_GATEWAY );
 $ipsp   = new Ipsp_Api( $client );

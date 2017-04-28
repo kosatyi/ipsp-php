20ed4c8f7ca6a6db726afb9ef77309a04e9a0e32
Stepan Kosatyi <stepan@kosatyi.com>
2017-04-28 17:48:28 +0300

    refactor docs page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
 (function(){
+
+    this.noopElement = document.createElement('span');
+
     this.toArray = function(value){
         return Array.prototype.slice.call(value);
     };
-    this.find = function(selector){
+    this.findAll = function(selector){
         return toArray(document.querySelectorAll(String(selector)));
+    };
+    this.find = function(selector){
+        return document.querySelector(String(selector)) || noopElement;
     };
 })();
 
+
+(function(){
+    find('.page-sidebar-toggle').addEventListener('click',function(ev){
+        ev.preventDefault();
+        document.body.classList.toggle('sidebar-show');
+    });
+})();
+
+
 (function(){
     var href = location.href;
-    find('[href],[data-rel]').filter(function(el,expr){
+    findAll('[href],[data-rel]').filter(function(el,expr){
         expr = el.getAttribute('data-rel');
         return expr ? href.match(expr) : el.href ? href.indexOf(el.href) !== -1 : false;
     }).map(function(el){
--- index.md
+++ index.md
@@ -1,7 +1,7 @@
 # Payment service provider
+
 A payment service provider (PSP) offers shops online services for accepting electronic payments by a variety of payment methods including credit card, bank-based payments such as direct debit, bank transfer, and real-time bank transfer based on online banking. Typically, they use a software as a service model and form a single payment gateway for their clients (merchants) to multiple payment methods.
 [read more](https://en.wikipedia.org/wiki/Payment_service_provider)
-
 
 {% include navigation.html base_url="/" max_depth=1 %}
 

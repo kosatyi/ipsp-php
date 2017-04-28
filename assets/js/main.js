24ce7d676f815ef42e58feac6daa15566550177f
Stepan Kosatyi <stepan@kosatyi.com>
2017-04-28 17:18:24 +0300

    refactor docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 (function(){
+    this.toArray = function(value){
+        return Array.prototype.slice.call(value);
+    };
+    this.find = function(selector){
+        return toArray(document.querySelectorAll(String(selector)));
+    };
+})();
 
-
-
+(function(){
+    var href = location.href;
+    find('[href],[data-rel]').filter(function(el,expr){
+        expr = el.getAttribute('data-rel');
+        return expr ? href.match(expr) : el.href ? href.indexOf(el.href) !== -1 : false;
+    }).map(function(el){
+        el.classList.add('active');
+    });
 })();

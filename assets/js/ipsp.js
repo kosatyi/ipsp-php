53362fab17c8e735c182b8a36f0d5c90b4b29189
Stepan Kosatyi <stepan@kosatyi.com>
2017-05-30 12:35:55 +0300

    refacor project

--- assets/js/ipsp.js
+++ assets/js/ipsp.js
@@ -1,103 +1,159 @@
-(function($){
-    $.PortalApi = function(){
+(function ($) {
+    $.fn.serializeObject = function () {
+        var result = {};
+        var extend = function (i, element) {
+            var node = result[element.name];
+            if ('undefined' !== typeof node && node !== null) {
+                if ($.isArray(node)) {
+                    node.push(element.value);
+                } else {
+                    result[element.name] = [node, element.value];
+                }
+            } else {
+                result[element.name] = element.value;
+            }
+        };
+        $.each(this.serializeArray(), extend);
+        return result;
+    };
+})(jQuery);
+
+(function ($) {
+    /**
+     *
+     * @constructor
+     */
+    $.PortalApi = function () {
         this.init();
     };
     $.PortalApi.prototype = {
-        origin:'https://portal.fondy.eu',
-        endpoint:{
-            portal  : '/mportal',
-            gateway : '/mportal/#/connector/52',
-            google  : '/api/account/registration/google/',
+        origin: 'https://portal.fondy.eu',
+        endpoint: {
+            portal: '/mportal',
+            gateway: '/mportal/#/connector/52',
+            google: '/api/account/registration/google/',
             facebook: '/api/account/registration/facebook/',
             linkedin: '/api/account/registration/linkedin/'
         },
-        loaded : false ,
-        frame:function(url){
+        loaded: false,
+        /**
+         *
+         * @param url
+         * @returns {*}
+         */
+        frame: function (url) {
             var defer = this.defer();
-            $('<iframe>').hide().attr('src',url).appendTo('body').on('load',function(ev){
+            $('<iframe>').hide().attr('src', url).appendTo('body').on('load', function (ev) {
                 defer.resolve(this);
-            }).on('error',function(){
+            }).on('error', function () {
                 defer.reject(this);
             });
             return defer;
         },
-        url:function(type,url){
-            return this.origin.concat(this.endpoint[type]||'/').concat(url||'');
+        /**
+         *
+         * @param type
+         * @param url
+         * @returns {string}
+         */
+        url: function (type, url) {
+            return this.origin.concat(this.endpoint[type] || '/').concat(url || '');
         },
-        init:function(){
+        /**
+         *
+         */
+        init: function () {
             this.wrapper = $(this);
-            this.frame(this.url('gateway')).then($.proxy(this,'load'));
+            this.frame(this.url('gateway')).then($.proxy(this, 'load'));
         },
-        load:function(frame){
+        /**
+         *
+         * @param frame
+         */
+        load: function (frame) {
             this.connector = new $.Connector(frame.contentWindow);
-            this.connector.action('load',$.proxy(this,'ready'));
+            this.connector.action('load', $.proxy(this, 'ready'));
         },
-        ready:function(ev,data){
+        /**
+         *
+         * @param ev
+         * @param data
+         */
+        ready: function (ev, data) {
             this.loaded = true;
             this.wrapper.trigger('portal.api').off('portal.api');
         },
-        scope:function(callback){
-            if( this.loaded ) return callback.call(this);
-            else this.wrapper.on('portal.api', $.proxy(callback,this) );
+        /**
+         *
+         * @param callback
+         * @returns {*}
+         */
+        scope: function (callback) {
+            if (this.loaded) return callback.call(this);
+            else this.wrapper.on('portal.api', $.proxy(callback, this));
         },
-        defer:function(){
+        /**
+         *
+         * @returns {$.Deferred}
+         */
+        defer: function () {
             return $.Deferred();
         },
-        request:function(model,method,params){
-            var defer   = this.defer();
-            var data    = {};
-            data.uid    = this.connector.getUID();
+        /**
+         *
+         * @param model
+         * @param method
+         * @param params
+         * @returns {defer}
+         */
+        request: function (model, method, params) {
+            var defer = this.defer();
+            var data = {};
+            data.uid = this.connector.getUID();
             data.action = model;
             data.method = method;
             data.params = params || {};
-            this.connector.send('request',data);
-            this.connector.action(data.uid,$.proxy(function( ev , response ){
-                defer[ response.error ? 'rejectWith' : 'resolveWith' ]( this , [ response ] );
-            },this));
+            this.connector.send('request', data);
+            this.connector.action(data.uid, $.proxy(function (ev, response) {
+                defer[response.error ? 'rejectWith' : 'resolveWith'](this, [response]);
+            }, this));
             return defer;
         },
-        batch:function(list){
-            var prop,request = [];
-            for(prop in list){
-                if(list.hasOwnProperty(prop)){
-
-                }
-            }
-            list.forEach(function(item){
-
-            })
-        },
-        session:function(){
-            return this.request('api.account','check');
+        /**
+         *
+         * @returns {defer}
+         */
+        session: function () {
+            return this.request('api.account', 'check');
         }
     };
 })(jQuery);
 
-(function($){
+(function ($) {
     $.ejs.basePath = '/assets/html';
 })(jQuery);
 
-(function($){
+(function ($) {
     var list = {};
-    var toArray = function(obj){
+    var toArray = function (obj) {
         return Array.prototype.slice.call(obj);
     };
-    $.addControl = function(name,callback){
+    $.addControl = function (name, callback) {
         list[name] = callback;
     };
-    $.bindControl = function(selector,name){
-        toArray(document.querySelectorAll(selector)).forEach(function(el,control){
+    $.bindControl = function (selector, name) {
+        toArray(document.querySelectorAll(selector)).forEach(function (el, control) {
             control = el.hasAttribute('control') ? el.getAttribute('control').split(',') : [];
-            if(control.indexOf(name)<0)  control.push(name);
-            el.setAttribute('control',control.join(','));
+            if (control.indexOf(name) < 0) control.push(name);
+            el.setAttribute('control', control.join(','));
         });
     };
-    $.initControls = function(){
-        toArray(document.querySelectorAll('[control]')).forEach(function(el,control,item){
-            item    = $(el);
+    $.initControls = function () {
+        toArray(document.querySelectorAll('[control]')).forEach(function (el, control, item) {
+            item = $(el);
             control = el.getAttribute('control').split(',');
-            control.forEach(function(name){
-                if(list.hasOwnProperty(name))
+            control.forEach(function (name) {
+                if (list.hasOwnProperty(name))
                     list[name](item);
             });
             el.removeAttribute('control');
@@ -105,51 +161,62 @@
     };
 })(jQuery);
 
-(function($){
+(function ($) {
     $.api = new $.PortalApi();
-    $.ejs.addHelper('api',$.api);
+    $.ejs.addHelper('api', $.api);
 })(jQuery);
 
-(function($){
-
-    $.PortalApi.prototype.account = function(email,password){
-        var method = password ? 'login' : 'registration';
-        return this.request('api.account',method,{
-            email:email,
-            password:password
-        });
+(function ($) {
+    /**
+     *
+     * @param form
+     * @returns {*}
+     */
+    $.PortalApi.prototype.account = function (form) {
+        var method = form.password ? 'login' : 'registration';
+        return this.request('api.account', method, form);
     };
 
-    $.addControl('signup',function(element){
-        var render   = function(template,data){
+    $.addControl('signup', function (element) {
+        var render = function (template, data) {
             element.html($.ejs(template).render(data));
         };
-        var success  = function(){
+        var success = function () {
             $.when(
-                this.request('api.account.milestone','get',{}),
-                this.request('api.merchant','list',{})
-            ).done(function(milestone,merchants){
-                render('/account',{
-                    milestone:milestone,
-                    merchants:merchants
+                this.request('api.account.milestone', 'get', {}),
+                this.request('api.merchant', 'list', {})
+            ).done(function (milestone, merchants) {
+                render('/account', {
+                    milestone: milestone,
+                    merchants: merchants
                 });
             });
         };
-        var submit   = function(ev,form){
+        var submit = function (ev, form) {
             ev.preventDefault();
-            form = new FormData(this);
-            $.api.account(form.get('email'),form.get('password')).fail(function(data){
-                render('/signup',{
-                    login: true ,
-                    email: form.get('email'),
+            form = $(this).serializeObject();
+            $.api.account(form).fail(function (data) {
+                render('/signup', {
+                    login: true,
+                    form: form,
                     error: data.error
                 });
-            }).done(success);
+            }).done(function (data) {
+                if (data.two_factor) {
+                    render('/signup', {
+                        login: true,
+                        two_factor: true,
+                        form: form
+                    });
+                } else {
+                    success.call(this, data);
+                }
+            });
         };
-        element.on('submit','form',submit);
-        $.api.scope(function(){
-            this.session().fail(function(){
-                render('/signup',{login:false});
+        element.on('submit', 'form', submit);
+        $.api.scope(function () {
+            this.session().fail(function () {
+                render('/signup', {login: false});
             }).done(success);
         });
     });
@@ -157,16 +224,12 @@
 })(jQuery);
 
 
-(function($){
+(function ($) {
     $.initControls();
 })(jQuery);
 
 
-
-
-(function($){
-
-
+(function ($) {
 
 
 })(jQuery);

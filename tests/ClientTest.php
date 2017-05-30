b1ed9af93cfeb64818cbfeca9e5f0c89cb8a6334
Stepan Kosatyi <stepan@kosatyi.com>
2017-05-30 15:36:04 +0300

    refactor tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,13 +2,14 @@
 
 require('autoload.php');
 
-use PHPUnit\Framework\TestCase;
-
-final class ClientTest extends TestCase
+final class ClientTest extends PHPUnit_Framework_TestCase
 {
     public function testCreateClientInstance()
     {
+
         $client = new Ipsp_Client( 1000 , 'test', 'api.fondy.eu' );
+
         $this->assertTrue( $client instanceof Ipsp_Client );
+
     }
 }
